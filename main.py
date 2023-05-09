a268430c0ce231cd933ad2b1ef8f69410690ae76
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-05-10 00:05:51 +0200

    work finish

--- main.py
+++ main.py
@@ -11,15 +11,13 @@ NORTH = 0
 EAST = 1
 SOUTH = 2
 WEST = 3
-SQUARE_SIZES = [5, 9, 7, 5, 4, 3]
+SQUARE_SIZES = [4, 7, 6, 5, 4, 3]
 
 SCREEN_HEIGHT = sum(SQUARE_SIZES) * 2 + 1
 SCREEN_WIDTH = int(SCREEN_HEIGHT * 1.1)
 
 MAZE_WIDTH = 21
 MAZE_HEIGHT = 11
-myMaze = [[False] * MAZE_WIDTH for _ in range(MAZE_HEIGHT)]
-myHero = [1, 3, 1]  # [y, x, direction]
 
 
 def buildMaze(maze):
@@ -27,7 +25,7 @@ def buildMaze(maze):
         for column in range(MAZE_WIDTH):
             if (row == 0) or (row == MAZE_HEIGHT - 1) or (column == 0) or (column == MAZE_WIDTH - 1):
                 maze[row][column] = True
-            if (row % 3 == 0) and (column % 3 == 0):
+            if (row % 2 == 0) and (column % 2 == 0):
                 maze[row][column] = True
 
 
@@ -35,7 +33,7 @@ def displayMaze2D(maze, hero):
     for row in range(MAZE_HEIGHT):
         for column in range(MAZE_WIDTH):
             if hero[0] == row and hero[1] == column:
-                print("0", end="")
+                print(hero[2], end="")
             elif maze[row][column]:
                 print("X", end="")
             else:
@@ -54,8 +52,21 @@ def createEmptyScreen():
     return [[" "] * SCREEN_WIDTH for _ in range(SCREEN_HEIGHT)]
 
 
+def isOutOfMaze(hero, distance):
+    if hero[2] == NORTH:
+        return hero[0] - distance < 0 or hero[0] - distance >= MAZE_HEIGHT
+    elif hero[2] == EAST:
+        return hero[1] + distance < 0 or hero[1] + distance >= MAZE_WIDTH
+    elif hero[2] == SOUTH:
+        return hero[0] + distance < 0 or hero[0] + distance >= MAZE_HEIGHT
+    elif hero[2] == WEST:
+        return hero[1] - distance < 0 or hero[1] - distance >= MAZE_WIDTH
+
+
 def addSquaresByDistanceInScreen(hero, maze, screen, distance):
     if (distance > 1) and getSquareOfPositionRelativeToHero(hero, maze, 0, 1):
+        return
+    if isOutOfMaze(hero, distance):
         return
     leftSquareIsAWall = getSquareOfPositionRelativeToHero(hero, maze, -1, distance)
     frontSquareIsAWall = getSquareOfPositionRelativeToHero(hero, maze, 0, distance)
@@ -106,7 +117,7 @@ def addSquaresByDistanceInScreen(hero, maze, screen, distance):
             for i in range(previousSquareTotalSize, previousSquareTotalSize + squareSize):
                 screen[i][i] = LEFT_ROOF
         else:
-            for i in range(previousSquareTotalSize, previousSquareTotalSize + squareSize):
+            for i in range(previousSquareTotalSize + squareSize):
                 screen[SCREEN_HEIGHT - previousSquareTotalSize - squareSize - 1][i] = CENTER_GROUND
         if rightSquareIsAWall:
             for i in range(previousSquareTotalSize + squareSize,
@@ -115,7 +126,7 @@ def addSquaresByDistanceInScreen(hero, maze, screen, distance):
             for i in range(previousSquareTotalSize, previousSquareTotalSize + squareSize):
                 screen[i][SCREEN_WIDTH - 1 - i] = RIGHT_ROOF
         else:
-            for i in range(previousSquareTotalSize, previousSquareTotalSize + squareSize):
+            for i in range(previousSquareTotalSize + squareSize):
                 screen[SCREEN_HEIGHT - previousSquareTotalSize - squareSize - 1][
                     SCREEN_WIDTH - i - 1] = CENTER_GROUND
         for i in range(previousSquareTotalSize + squareSize,
@@ -152,6 +163,60 @@ def printScreen(screen):
         print()
 
 
+def moveHeroForward(hero, maze):
+    if hero[2] == NORTH:
+        if not maze[hero[0] - 1][hero[1]]:
+            hero[0] -= 1
+    elif hero[2] == EAST:
+        if not maze[hero[0]][hero[1] + 1]:
+            hero[1] += 1
+    elif hero[2] == SOUTH:
+        if not maze[hero[0] + 1][hero[1]]:
+            hero[0] += 1
+    elif hero[2] == WEST:
+        if not maze[hero[0]][hero[1] - 1]:
+            hero[1] -= 1
+
+
+def moveHeroBack(hero, maze):
+    if hero[2] == NORTH:
+        if not maze[hero[0] + 1][hero[1]]:
+            hero[0] += 1
+    elif hero[2] == EAST:
+        if not maze[hero[0]][hero[1] - 1]:
+            hero[1] -= 1
+    elif hero[2] == SOUTH:
+        if not maze[hero[0] - 1][hero[1]]:
+            hero[0] -= 1
+    elif hero[2] == WEST:
+        if not maze[hero[0]][hero[1] + 1]:
+            hero[1] += 1
+
+
+def turnHeroRight(hero):
+    hero[2] = (hero[2] + 1) % 4
+
+
+def turnHeroLeft(hero):
+    hero[2] = (hero[2] - 1) % 4
+
+
+myMaze = [[False] * MAZE_WIDTH for _ in range(MAZE_HEIGHT)]
+myHero = [2, 3, 2]  # [y, x, direction]
 buildMaze(myMaze)
-displayMaze2D(myMaze, myHero)
-displayMaze3D(myMaze, myHero)
+
+running = True
+while running:
+    displayMaze2D(myMaze, myHero)
+    displayMaze3D(myMaze, myHero)
+    command = input("Enter command: ")
+    if command == "x":
+        running = False
+    elif command == "z":
+        moveHeroForward(myHero, myMaze)
+    elif command == "d":
+        turnHeroRight(myHero)
+    elif command == "s":
+        moveHeroBack(myHero, myMaze)
+    elif command == "q":
+        turnHeroLeft(myHero)
